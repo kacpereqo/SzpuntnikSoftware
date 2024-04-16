2d0646e7587adbfd963185593a638c0d65d942bb
kacpereqo <remzak.k@gmail.com>
2024-04-16 19:45:28 +0200

    feat: working imu

---
+++ src/LSM6DS33.hpp
@@ -0,0 +1,75 @@
+#ifndef LSM6DS33_H
+#define LSM6DS33_H
+
+/*
+DATA SHEET:
+https://pdf1.alldatasheet.com/datasheet-pdf/download/880694/STMICROELECTRONICS/LSM6DS33.html
+*/
+
+#include <LSM6.h>
+
+struct LSM6DS33
+{
+    enum class GyroScale : uint8_t
+    {
+        dps125,
+        dps245,
+        dps500,
+        dps1000,
+        dps2000,
+    };
+
+    enum class GyroRate : uint16_t
+    {
+        Hz13,
+        Hz26,
+        Hz52,
+        Hz104,
+        Hz208,
+        Hz416,
+        Hz833,
+        Hz1660,
+
+    };
+
+    enum class AccelScale : uint8_t
+    {
+        g2,
+        g16,
+        g4,
+        g8,
+    };
+
+    enum class AccelRate : uint16_t
+
+    {
+        off,
+        Hz13,
+        Hz26,
+        Hz52,
+        Hz104,
+        Hz208,
+        Hz416,
+        Hz833,
+        Hz1660,
+        Hz3330,
+        Hz6660,
+    };
+
+    using Data = std::array<float, 3>;
+
+    LSM6DS33();
+
+    void configureGyro(GyroScale scale, GyroRate rate);
+    void configureAccel(AccelScale scale, AccelRate rate);
+
+    Data readAccel();
+    Data readGyro();
+
+private:
+    LSM6 imu;
+    GyroScale current_gyro_scale = GyroScale::dps245;
+    AccelScale current_accel_scale = AccelScale::g2;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
-#include "gy-521.hpp"
-#include <Arduino.h>
 
-void setup(void)
+#include <Wire.h>
+#include "LSM6DS33.hpp"
+
+void setup()
 {
   Serial.begin(115200);
-  mpu_init();
+  Wire.begin();
+  while (!Serial)
+    ;
 }
 
 void loop()
 {
-  mpu_loop();
+  static LSM6DS33 imu;
+
+  imu.configureAccel(LSM6DS33::AccelScale::g16, LSM6DS33::AccelRate::Hz1660);
+
+  auto accel = imu.readGyro();
+  Serial.print("gyro: ");
+  Serial.print(accel[0]);
+  Serial.print(", ");
+  Serial.print(accel[1]);
+  Serial.print(", ");
+  Serial.println(accel[2]);
+  delay(100);
 }
