85e573a14fa5aa6a3f3a52ae62ab39d91f6dd817
kacpereqo <remzak.k@gmail.com>
2024-04-18 16:33:39 +0200

    chore: vue init

--- plotter/main.py
+++ plotter/main.py
@@ -7,31 +7,34 @@ from fastapi import FastAPI, WebSocket
 
 app = FastAPI()
 
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     config = configparser.ConfigParser()
-    config.read('platformio.ini')
 
-    env_name = config.sections()[0].split(':')[1].replace('_', ' ')
+    config.read("platformio.ini")
+
+    env_name = config.sections()[0].split(":")[1].replace("_", " ")
     port = None
 
     ports = list(serial.tools.list_ports.comports())
     for p in ports:
-        if p.product is None: 
+        if p.product is None:
             continue
         if p.product.lower() == env_name:
             port = p.device
 
     if port is None:
-        print("No device found")
+        port = config[config.sections()[0]]["upload_port"]
     else:
         print(f"Found device at {port}")
 
     ser = serial.Serial(port, 115200)
     print("Connected to serial port")
+    print(ser.readline().decode("utf-8").strip())
     while True:
-        data = ser.readline().decode('utf-8').strip()
+        data = ser.readline().decode("utf-8").strip()
         await websocket.send_text(data)
 
 
