32d7de74bfb22e6240b5de855959fd677740f912
kacpereqo <remzak.k@gmail.com>
2024-04-25 00:16:03 +0200

    to calibrate

--- src/imu.cpp
+++ src/imu.cpp
@@ -17,18 +17,21 @@ void Imu::calibrate()
 void Imu::calculateRollAndPitch()
 {
 
-    constexpr float GYRO_SENSITIVITY = 0.92;
-    constexpr float ACCEL_SENSITIVITY = 0.08;
+    constexpr float GYRO_SENSITIVITY = 0.88;
+    constexpr float ACCEL_SENSITIVITY = 0.12;
 
     // assert(ACCEL_SENSITIVITY + GYRO_SENSITIVITY == 1);
 
     static long lastUpdate = 0;
 
-    float dt = (millis() - lastUpdate) / 1000.0;
+    double dt = (millis() - lastUpdate) / 1000.0;
     lastUpdate = millis();
 
     const float pitchAcc = atan2(accel.y, sqrt(accel.x * accel.x + accel.z * accel.z)) * RAD_TO_DEG;
     const float rollAcc = atan2(accel.x, sqrt(accel.y * accel.y + accel.z * accel.z)) * RAD_TO_DEG;
+
+    // this->pitch = lowPassFilter(pitchAcc * ACCEL_SENSITIVITY + (this->pitch + gyro.x * dt) * GYRO_SENSITIVITY, this->pitch, 0.1);
+    // this->roll = lowPassFilter(rollAcc * ACCEL_SENSITIVITY + (this->roll + gyro.y * dt) * GYRO_SENSITIVITY, this->pitch, 0.1);
 
     this->pitch = pitchAcc * ACCEL_SENSITIVITY + (this->pitch + gyro.x * dt) * GYRO_SENSITIVITY;
     this->roll = rollAcc * ACCEL_SENSITIVITY + (this->roll + gyro.y * dt) * GYRO_SENSITIVITY;
@@ -54,13 +57,28 @@ void Imu::calculateYaw()
     constexpr float declinationAngle = 0.593; // in radians
 
     // normalize magnetometer values to [-1, 1]
-    Vec3<float> normalized_mag = {
-        (float)(mag.x * sin(pitch * DEG_TO_RAD) - min_x) / (max_x - min_x) * 2 - 1,
-        (float)(mag.y * sin(roll * DEG_TO_RAD) - min_y) / (max_y - min_y) * 2 - 1,
-    };
+    Vec2<float> normalized_mag = {
+        (float)(mag.x * cos(roll * DEG_TO_RAD) - min_x) / (max_x - min_x) * 2 - 1,
+        (float)(mag.y * cos(pitch * DEG_TO_RAD) - min_y) / (max_y - min_y) * 2 - 1,
 
-    const float heading = atan2(normalized_mag.y, normalized_mag.x) + declinationAngle;
-    const float headingDegrees(heading * RAD_TO_DEG);
+    };
+    // Serial.print(.x);
+    // Serial.print(",");
+    // Serial.print(mag.y);
+    // Serial.print(",");
+
+    Vec2<float> normalized_mag_z = {
+        (float)(mag.z * sin(pitch * DEG_TO_RAD) - min_z) / (max_z - min_z) * 2 - 1,
+        (float)(mag.z * sin(roll * DEG_TO_RAD) - min_z) / (max_z - min_z) * 2 - 1};
+
+    Serial.print("Normalized mag: ");
+    Serial.print(normalized_mag_z.x);
+    Serial.print(",");
+    Serial.print(normalized_mag_z.y);
+    Serial.print("|");
+
+    const float heading = atan2(normalized_mag.y * normalized_mag_z.y, normalized_mag.x * normalized_mag_z.x) + declinationAngle;
+    const float headingDegrees = heading * RAD_TO_DEG;
 
     this->yaw = headingDegrees * MAG_SENSIVITY + gyro.z * GYRO_SENSITIVITY;
 }
@@ -87,16 +105,14 @@ void Imu::readings()
     // Serial.print("Pitch: ");
     Serial.print(this->pitch);
     Serial.print(",");
-    this->filtered_pitch = lowPassFilter(this->pitch, this->filtered_pitch, 0.05);
-    Serial.print(this->filtered_pitch);
-    Serial.println();
     // Serial.print(" Roll: ");
-    // Serial.print(this->roll);
-    // Serial.print(",");
+    Serial.print(this->roll);
+    Serial.print(",");
     // Serial.print(" Yaw: ");
-    // Serial.print(this->yaw);
+    Serial.print(this->yaw);
+    Serial.println();
     // Serial.print(",");
     // Serial.print(" Preassure: ");
     // Serial.println(this->preassure);
-    delay(1000 / 30);
+    delay(1000 / 100);
 }
